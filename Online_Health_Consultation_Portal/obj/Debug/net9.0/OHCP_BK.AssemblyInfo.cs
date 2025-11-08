cacc04d5e269e892d553b5debea9b35167e14d98
Davidle96 <tranquocdatnt1990@gmail.com>
2025-11-09 02:34:04 +0700

    update video call

--- Online_Health_Consultation_Portal/obj/Debug/net9.0/OHCP_BK.AssemblyInfo.cs
+++ Online_Health_Consultation_Portal/obj/Debug/net9.0/OHCP_BK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHCP_BK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44b20990513c3b1c04ff79d3b91d6cdea10f930")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHCP_BK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHCP_BK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
