118432b65aaf8fdcc17aa47db021d38cfb62e0ea
dohung95 <dohungjp1995@gmail.com>
2025-11-09 12:29:14 +0700

    update

--- Online_Health_Consultation_Portal/obj/Debug/net9.0/OHCP_BK.AssemblyInfo.cs
+++ Online_Health_Consultation_Portal/obj/Debug/net9.0/OHCP_BK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHCP_BK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+047da0358c27e763d1d36b4d265bdaa080cfa09e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e1dda570506f3487850129dc7e777ac5d92993")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHCP_BK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHCP_BK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
