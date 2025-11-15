ac85ea9f67e28b6a2dc0c3fef1f51d8a99ddc5c4
Ndhhiep <hiep.ndh2376@sinhvien.hoasen.edu.vn>
2025-11-15 19:42:31 +0700

    hiep

--- Online_Health_Consultation_Portal/Controllers/PatientController_hiep.cs
+++ Online_Health_Consultation_Portal/Controllers/PatientController_hiep.cs
@@ -9,12 +9,12 @@ namespace OHCP_BK.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
-    public class PatientController_hiep : ControllerBase
+    public class PatientController : ControllerBase
     {
         private readonly OHCPContext _context;
-        private readonly ILogger<PatientController_hiep> _logger;
+        private readonly ILogger<PatientController> _logger;
 
-        public PatientController_hiep(OHCPContext context, ILogger<PatientController_hiep> logger)
+        public PatientController(OHCPContext context, ILogger<PatientController> logger)
         {
             _context = context;
             _logger = logger;
