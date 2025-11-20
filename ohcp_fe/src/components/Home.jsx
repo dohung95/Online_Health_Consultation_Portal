219dd572b16735c1a0124196f5049c6538f70b25
dohung95 <dohungjp1995@gmail.com>
2025-11-20 13:48:07 +0700

    update giao diện

--- ohcp_fe/src/components/Home.jsx
+++ ohcp_fe/src/components/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import './Css/Home.css';
+import Navbar from './Navbar';
+import Loading from './Loading';
 
 function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Giả lập thời gian load trang (có thể thay bằng logic load data thực tế)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500); // 1.5 giây
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <div className="section">
-      <h1>Welcome to Online Health Consultation Portal</h1>
-      <p>Enhance your healthcare experience with virtual consultations, health records, and more!</p>
+    <div className="Background_Home" >
+       <div className='container'>
+          <Navbar />
+        </div>
+
     </div>
   );
 }
